35d19a1afa78f4fc213b556d8ca10451fae8bd1f
MikelSL9 <mikelreguers@gmail.com>
2025-03-23 20:03:22 +0100

    adaptar estils llista apats

--- CAMPtina_Frontend/projecte-camptina/src/pagines/apats/apats-seccio/LlistaApats.jsx
+++ CAMPtina_Frontend/projecte-camptina/src/pagines/apats/apats-seccio/LlistaApats.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 
 export const LlistaApats = ({ apats, alEliminarApat }) => {
     const [esTancat, setEsTancat] = useState(false)
-
+    
     const tancarFinestra = () => {
         setEsTancat(!esTancat);
     }
@@ -12,13 +12,15 @@ export const LlistaApats = ({ apats, alEliminarApat }) => {
         if (esTancat) {
             const section = document.getElementById('id_section_llista_apats');
             const article = document.getElementById('id_article_llista_apats');
-            article.setAttribute('style', 'display: none;');
-            section.setAttribute('style', 'height: 70px');
+            //article.setAttribute('style', 'display: none;');
+            //section.setAttribute('style', 'height: 70px');
+            article.style.display = 'none';
         } else {
             const section = document.getElementById('id_section_llista_apats');
             const article = document.getElementById('id_article_llista_apats');
-            article.removeAttribute('style');
-            section.removeAttribute('style');
+            //article.removeAttribute('style');
+            //section.removeAttribute('style');
+            article.style.display = '';
         }
     }, [esTancat])
 
@@ -40,6 +42,7 @@ export const LlistaApats = ({ apats, alEliminarApat }) => {
     const className_li_nom_llista = 'cn-li-nom-llista-apats';
     const className_li_categoria_llista = 'cn-li-categoria-llista-apats';
     const className_li_descripcio_llista = 'cn-li-descripcio-llista-apats';
+    const className_li_eliminar_llista = 'cn-li-eliminar-llista-apats';
 
     const className_li_1 = 'cn-li-1-llista-apats';
     const className_li_2 = 'cn-li-2-llista-apats';
@@ -55,7 +58,10 @@ export const LlistaApats = ({ apats, alEliminarApat }) => {
 
     return (
         <>
-            <section className={className_section} id={id_section}>
+            <section 
+                className={`${className_section} ${apats.length > 0 ? 'plena' : ''} `} 
+                id={id_section}
+            >
                 <article className={className_article_nom}>
                     <h3 className={className_h3_nom}>{nomSeccio}</h3>
                     <button
@@ -76,6 +82,7 @@ export const LlistaApats = ({ apats, alEliminarApat }) => {
                         <li className={className_li_nom_llista}>{llistaNom}</li>
                         <li className={className_li_categoria_llista}>{llistaCategoria}</li>
                         <li className={className_li_descripcio_llista}>{llistaDescripcio}</li>
+                        <li className={className_li_eliminar_llista}></li>
                     </ul>
                     {apats.map((apat) => (
                             <ul key={apat.id} className={className_ul_llista}>
@@ -83,22 +90,22 @@ export const LlistaApats = ({ apats, alEliminarApat }) => {
                                 <li className={className_li_nom_llista}>{apat.nom}</li>
                                 <li className={className_li_categoria_llista}>{apat.categoria}</li>
                                 <li className={className_li_descripcio_llista}>{apat.descripcio}</li>
-                                <li>
+                                <li className={className_li_eliminar_llista}>
                                     <button 
                                         onClick={() => alEliminarApat(apat.id)}
                                         className={className_bttn_eliminar}
                                     >
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" 
-                                        stroke="currentColor" className="size-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 
-                                            0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 
-                                            0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 
-                                            0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 
-                                            0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 
-                                            2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                        </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" 
+                                            stroke="currentColor" className="size-6">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 
+                                                0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 
+                                                0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 
+                                                0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 
+                                                0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 
+                                                2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
                                     </button>
-                                </li>
+                                </li> 
                             </ul>           
                     ))}
                 </article>
