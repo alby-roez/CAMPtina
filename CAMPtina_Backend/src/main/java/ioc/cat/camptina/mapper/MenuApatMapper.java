d4f265b7448666144ba96b466299bf50714037fc
Laurelindorean <kguinera@hotmail.com>
2025-04-09 23:23:55 +0200

    refactor

--- CAMPtina_Backend/src/main/java/ioc/cat/camptina/mapper/MenuApatMapper.java
+++ CAMPtina_Backend/src/main/java/ioc/cat/camptina/mapper/MenuApatMapper.java
@@ -14,11 +14,11 @@ public interface MenuApatMapper {
 
 	@Mapping(source = "menu.id", target = "menuId")
 	@Mapping(source = "apat.id", target = "apatId")
-	MenuApatDTO menuApatEntityToMenuApatDTO(MenuApatEntity menuApatEntity);
+	MenuApatDTO menuApatEntityToMenuApatDTO(MenuApatEntity entity);
 
 	@Mapping(target = "menu", ignore = true)
 	@Mapping(target = "apat", ignore = true)
 	@Mapping(target = "id", expression = "java(new MenuApatId(dto.getMenuId(), dto.getApatId()))")
-	MenuApatEntity menuApatDTOToMenuApatEntity(MenuApatDTO menuApatDTO);
+	MenuApatEntity menuApatDTOToMenuApatEntity(MenuApatDTO dto);
 
 }
