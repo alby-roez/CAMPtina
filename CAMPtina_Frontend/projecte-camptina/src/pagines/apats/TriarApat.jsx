036f8c76dfc5ecea84c0cc732747055e2697cb63
Alby Roez <albyroez@gmail.com>
2025-05-02 14:14:58 +0200

    Reserva feta

--- CAMPtina_Frontend/projecte-camptina/src/pagines/apats/TriarApat.jsx
+++ CAMPtina_Frontend/projecte-camptina/src/pagines/apats/TriarApat.jsx
@@ -1,20 +1,27 @@
 import { Navegacio } from '../../header/Navegacio.jsx'
 import { Peu } from '../../footer/Peu.jsx'
-import triar_apat from '../../assets/menjar.mp4'
+//import triar_apat from '../../assets/menjar.mp4'
+import eat from '../../assets/eat.mp4'
 import { SeccioTriarApat } from './triar-seccio/SeccioTriarApat.jsx'
+import { useState } from 'react'
+import { ReservaFeta } from './triar-seccio/ReservaFeta.jsx'
 
 export default function TriarApat() {
     const className_main = 'cn-main-navegacio';
     const className_section_video = 'cn-section-video-navegacio';
+
+    const [reservaState, setReservaState] = useState(true)
 
     return (
         <>
             <Navegacio />
             <main className={className_main}>
                 <section className={className_section_video}>
-                    <video id='id_video_triar_apat' src={triar_apat} autoPlay loop muted></video>
+                    <video id='id_video_triar_apat' src={eat} autoPlay loop muted></video>
                 </section>
-                <SeccioTriarApat />
+                { reservaState
+                    ? <ReservaFeta fn={setReservaState} state={reservaState} />
+                    : <SeccioTriarApat /> }
             </main>
             <Peu />
         </>
---
+++ CAMPtina_Frontend/projecte-camptina/src/pagines/apats/triar-seccio/ReservaFeta.jsx
@@ -0,0 +1,118 @@
+import './ReservaFeta.css'
+import { useState } from 'react'
+import { DeletePaperera, ExpandirIcona, ReduirIcona, UpdateLlapis } from '../../../Icones.jsx'
+
+const ApatsExpandir = () => {
+    return (
+        <div className='cn-div-apats-expandir-reduir-reserva-feta'>
+            <div className='cn-div-apats-expandir-reserva-feta'>
+                <div className='cn-container-h3-h4-reserva-feta'>
+                    <div className='cn-a-container-e-reserva-feta'>
+                        <h3>{'PRIMER:'}</h3>
+                    </div>
+                    <div className='cn-apat-container-e-reserva-feta'>
+                        <h4>{'APAT 1'}</h4>
+                    </div>
+                </div>
+                <div className='cn-apat-container-descripcio-e-reserva-feta'>
+                    <p>{'APAT 1 es una descripció'}</p>
+                </div>
+            </div>
+            <div className='cn-div-apats-expandir-reserva-feta'>
+                <div className='cn-container-h3-h4-reserva-feta'>
+                    <div className='cn-a-container-e-reserva-feta'>
+                        <h3>{'SEGON:'}</h3>
+                    </div>
+                    <div className='cn-apat-container-e-reserva-feta'>
+                        <h4>{'APAT 2'}</h4>
+                    </div>
+                </div>
+                <div className='cn-apat-container-descripcio-e-reserva-feta'>
+                    <p>{'APAT 2 es una descripcióAPAT 2 es una descripcióAPAT 2 es una descripcióAPAT 2 es una descripcióAPAT 2 es una descripció'}</p>
+                </div>
+            </div>
+            <div className='cn-div-apats-expandir-reserva-feta'>
+                <div className='cn-container-h3-h4-reserva-feta'>
+                    <div className='cn-a-container-e-reserva-feta'>
+                        <h3>{'POSTRES:'}</h3>
+                    </div>
+                    <div className='cn-apat-container-e-reserva-feta'>
+                        <h4>{'APAT 3'}</h4>
+                    </div>
+                </div>
+                <div className='cn-apat-container-descripcio-e-reserva-feta'>
+                    <p>{'APAT 3 es una descripció'}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const ApatsReduir = () => {
+    return (
+        <div className='cn-div-apats-expandir-reduir-reserva-feta'>
+            <h4>{'APAT 1'}</h4>
+            <h4>{'APAT 2'}</h4>
+            <h4>{'APAT 3'}</h4>
+        </div>
+    )
+}
+
+export const ReservaFeta = ({ fn, state }) => {
+
+    //const { torns, crearReserva } = useContext(DadesCamptinaContext);
+    //const { menuPrimer, menuSegon, menuPostres} = useMenus()
+
+    const maneigState = () => {
+        fn(!state)
+    }
+
+    const [cambi, setCambi] = useState(true)
+
+    const fn_cambiar = () => {
+        setCambi(!cambi)
+    }
+
+    return (
+        <section className='cn-section-reserva-feta'>
+            <article className='cn-article-titol-torn-reserva-feta'>
+                <h2>{'1 - 5 - 25 ----- 23:00'}</h2>
+            </article>
+            <article className='cn-article-contingut-reserva-feta'>
+                <div className='cn-div-actualitzar-eliminar-reserva-feta'>
+                    <button
+                        className='cn-bttn-actualitzar-eliminar-reserva-feta'
+                        name='nameBttnActualitzarReservaFeta'
+                        onClick={maneigState}
+                    >
+                        <UpdateLlapis />
+                    </button>
+                </div>
+                <div className='cn-div-apats-e-r-reserva-feta'>
+                    <div className='cn-div-container-titol-apats-reserva-feta'>
+                        <h3>{'APATS ESCOLLITS'}</h3>
+                        { cambi
+                        ? <button className='cn-bttn-cambiar-tamany-reserva-feta' name='nameBttnExpandir' onClick={fn_cambiar}>
+                            <ExpandirIcona />
+                            </button>
+                        : <button className='cn-bttn-cambiar-tamany-reserva-feta' name='nameBttnExpandir' onClick={fn_cambiar}>
+                            <ReduirIcona />
+                            </button> }
+                    </div>
+                    { cambi
+                    ? <ApatsReduir />
+                    : <ApatsExpandir /> }
+                </div>
+                <div className='cn-div-actualitzar-eliminar-reserva-feta'>
+                    <button
+                        className='cn-bttn-actualitzar-eliminar-reserva-feta'
+                        name='nameBttnEliminarReservaFeta'
+                        onClick={''}
+                    >
+                        <DeletePaperera />
+                    </button>
+                </div>
+            </article>
+        </section>
+    )
+}
